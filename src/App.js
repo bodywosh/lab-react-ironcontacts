6e550d26cbc05c46cc2c099a8fcb7f5a91b734a9
bodywosh <bodywashtv@gmail.com>
2022-04-05 16:09:21 +0200

    solved no css doe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import "./App.css";
+import { useState } from 'react'
+
+const contactsJson = require('./contacts.json');
+let remainingContacts = [...contactsJson]
+let contacts = remainingContacts.splice(0,5)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState(contacts)
+
+  const addContact = () => {  
+    contacts.push(remainingContacts.splice(Math.floor(Math.random()*remainingContacts.length),1)[0])
+    setData([...contacts])
+  }
+
+  const sortName = () => {  
+    contacts.sort(function (a, b) {
+      return a.name.localeCompare(b.name)
+    })   
+    setData([...contacts])
+  }
+
+  const sortPopularity = () => {  
+    contacts.sort(function (a, b) {
+      return a.popularity - b.popularity;
+    })
+    setData([...contacts])
+  }
+
+  const deleteContact = (id) => {
+    contacts = contacts.filter(item =>item.id !== id)
+    setData([...contacts])
+  }
+
+  return <div className="App">
+    <button onClick={addContact}>ADD RANDOM CONTACT</button>
+    <button onClick={sortName}>SORT BY NAME</button>
+    <button onClick={sortPopularity}>SORT BY POPULARITY</button>
+    <table>
+        <thead>
+            <tr>
+                <th>Picture</th>
+                <th>Name</th>
+                <th>Popularity</th>
+                <th>Won an Oscar</th>
+                <th>Won an Emmy</th>
+            </tr>
+        </thead>
+        <tbody>
+            {data.map((item)=>{ 
+            return <tr key={item.id}>
+                <td><img src={item.pictureUrl} alt="" height="120px"></img></td>
+                <td>{item.name}</td>
+                <td>{item.popularity}</td>
+                <td>{(item.wonOscar) ? '🏆' : null}</td>
+                <td>{(item.wonEmmy) ? '🏆' : null}</td>
+                <td><button onClick={()=>deleteContact(item.id)}>❌</button></td>
+            </tr>             
+            })}
+        </tbody>
+    </table>
+  </div>;
 }
 
 export default App;
